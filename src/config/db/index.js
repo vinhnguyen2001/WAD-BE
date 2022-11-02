8565fe1ed6c6329a3bea4abc3c41a5a2e1f2a6ff
vinhnguyen2001 <vinhnguyentran2001@gmail.com>
2022-11-02 15:41:12 +0700

    fix DB bug

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const props = require("../connect/properties");
 async function connect() {
-  console.log("props", props.DB);
+  // console.log("props", props.DB);
   try {
-    await mongoose.connect(props.DB, {
+    await mongoose.connect(process.env.DATABASE_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
